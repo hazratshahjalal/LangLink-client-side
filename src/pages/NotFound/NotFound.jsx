815b843be91a6419bcd1a89c7040f4a9312420ec
hazratshahjalal <hazratshahjalal503@gmail.com>
2023-06-08 00:32:12 +0600

    404 page added and return home option made

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const NotFound = () => {
+function Notfound() {
   return (
-    <div>
-      <h1>notfound page</h1>
-
+    <div className="my-7 px-20">
+      <div className="flex flex-col lg:flex-row justify-around items-center gap-10">
+        <div className="flex flex-col">
+          <h1 className="text-5xl text-emerald-700 font-black mb-5 ">You are Lost Buddy!</h1>
+          <p className="text-3xl font-bold py-8">
+            The page you have searching for is not found. Please click below
+            button to return home
+          </p>
+          <Link to={"/"}>
+            {" "}
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+              Return Home
+            </button>
+          </Link>
+        </div>
+        <div className="">
+          <img
+            className="w-[694px] h-[455px]"
+            src="https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
-};
+}
 
-export default NotFound;
+export default Notfound;
