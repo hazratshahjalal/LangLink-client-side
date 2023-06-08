65ef50ee7f370c178659045c48d94c797aab671c
hazratshahjalal <hazratshahjalal503@gmail.com>
2023-06-08 22:12:14 +0600

    fake json data created

---
+++ src/components/Class/Class.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Class = () => {
+  return (
+    <div>
+      classes
+    </div>
+  );
+};
+
+export default Class;
---
+++ src/components/Instructor/Instructor.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Instructor = () => {
+  return (
+    <div>
+      Instructors
+    </div>
+  );
+};
+
+export default Instructor;
